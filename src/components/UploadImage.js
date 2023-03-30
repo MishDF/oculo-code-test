0ca0a0362132902074396994095b7fd1a891b5c9
MishDF <mishd14@gmail.com>
2023-03-30 13:08:17 +1100

    Connect upload button to file picker and added array of uploaded images

--- src/components/UploadImage.js
+++ src/components/UploadImage.js
@@ -2,22 +2,38 @@ import React, { useState } from "react";
 
 function UploadImage() {
     const [selectedImage, setSelectedImage] = useState(null);
+    const [uploadedImages, setUploadedImages] = useState([]);
+
+
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        if(selectedImage){
+            //add on submit functionality
+            setUploadedImages([selectedImage]);
+        }
+    };
 
     return (
         <div className="UploadImage">
-            <div className="filePicker">
-                <input 
-                type="file" 
-                accept="image/*" 
-                data-testid="file-input"
-                onChange={(event) => {
-                    console.log(event.target.files[0]);
-                    setSelectedImage(event.target.files[0]);
-                  }}
-                />
+            <div className="imageForm">
+                <form onSubmit={handleSubmit}>
+                    <input 
+                    type="file" 
+                    accept="image/*" 
+                    data-testid="file-input"
+                    onChange={(event) => {
+                        setSelectedImage(event.target.files[0]);
+                    }}
+                    />
+                    <button type="submit" data-testid="submit-button">Upload Image</button>
+                </form>
             </div>
-            <div className="currentImage">
+            <div className="currentImage"> 
                 {selectedImage && <p data-testid="file-name">{selectedImage.name}</p>}
+            </div>
+            <div className="uploadedImages"> 
+                {uploadedImages[0] && <p data-testid="uploaded-name">{uploadedImages[0].name}</p>}
             </div>
         </div>
     );
--- src/components/UploadImage.test.js
+++ src/components/UploadImage.test.js
@@ -8,6 +8,12 @@ test('renders file input', () => {
   expect(fileInput).toBeInTheDocument();
 })
 
+test('renders upload button', () => {
+  render(<UploadImage/>);
+  const button = screen.getByText(/Upload image/i)
+  expect(button).toBeInTheDocument()
+});
+
 test('selected image is changed when fileInput changed', () => {
   render(<UploadImage/>);
   const fileInput = screen.getByTestId(/file-input/i);
@@ -22,4 +28,19 @@ test('selected image is changed when fileInput changed', () => {
   expect(fileName.textContent).toBe('test.jpg');
 })
 
-//test if set currentImage is called??
+test('uploaded image is changed when file submitted', () => {
+  render(<UploadImage/>);
+  const fileInput = screen.getByTestId(/file-input/i);
+
+  const file = new File(['test image content'], 'test.jpg', {
+    type: 'image/jpeg',
+  });
+  fireEvent.change(fileInput, { target: { files: [file] } });
+  
+  const button = screen.getByText(/Upload image/i);
+  fireEvent.click(button);
+
+  const fileName = screen.getByTestId('uploaded-name');
+
+  expect(fileName.textContent).toBe('test.jpg');
+})
